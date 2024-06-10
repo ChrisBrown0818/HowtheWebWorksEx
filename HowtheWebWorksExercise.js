c057d5f45cf07a6902a845d0ce8b1301c9c52672
chris <chris@KLB-87JFDISM.localdomain>
2024-06-10 09:04:30 -0400

    first commit

---
+++ HowtheWebWorksExercise.js
@@ -0,0 +1,35 @@
+//1. What is http?
+//Hypertext Transfer Protocal.  Structures the way we get data from or to a server.
+
+//2.What is a URL?
+//Stands for Uniform Resource Locator, its an address for an internet resource.
+
+//3.what is a DNS?
+//Domain Name System.  This is a system that takes human readable URL's and converts them into an ip address.
+
+//4.What is a query string.
+//their at the end of the url and specify specific things when getting information.  Such as making sure the page your going to is in english.
+
+
+//5. List two http verbs and their cases.
+//So far we just learned POST and GET. Get requests and recieves data and doesnt impact the sever while POST does have an impact on
+//the server.  POST sends some form of data to the server exmple being posting a comment on youtube.
+
+//6.  WHat is an Http request?
+//a request from client to server following the http structure.
+
+//7.What is an http response?
+//a response from a server to a client which also follows http protocol.
+
+//8. What is an http header? give a couple examples of request and response headers.
+//headers provide additional information about the request or the response. Requeast headers can be: Host or a User-Agent
+//Response Headers can be: Content Type or Last Modified.
+
+
+//9.What happens when you type a URL in a browser?
+//browser resolves the name into an ip address using DNS.
+//browser makes a requestto the IP address, including headers.
+//sever sends a response such as 200 (sucCess) or 404 (not found)
+//browser makes a DOM from the html, and finds other resources needed. (css, images etc.)
+//the browser makes separate http requests for those resources and recieves responces from the server for each.
+
